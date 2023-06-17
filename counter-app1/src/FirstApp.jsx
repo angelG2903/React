3213e132121fd2022a1d9a36578129896697cb9a
Angel Gabriel Muñoz Chavez <angel.chavez.amo@gmail.com>
2023-06-17 15:39:52 -0600

    useState

--- counter-app1/src/FirstApp.jsx
+++ counter-app1/src/FirstApp.jsx
@@ -1,9 +1,58 @@
+// Maneras de exportar un componente
 /* export function App() {
     return (<h1>Hola Mundo!!</h1>);
 } */
 
-export const App = () => {
+
+// ---------------------------------------------------------------------------
+// import { Fragment } from "react"
+// Importamos fragment para colocar mas de un elemento en el componente
+
+/* export const App = () => {
     return (
-        <h1>Hello World App</h1>
+        <Fragment>
+            <h1>Hello World App</h1>
+            <p>parrafo</p>
+        </Fragment>
     )
+} */
+
+// Aqui otra manera de ocupar un fragment sin la necesidad de exportarlo <> </>
+
+// Las propTypes son para definir el tipo a las props
+import PropTypes from 'prop-types';
+
+const newMessage = {
+    message: 'Hola Mundo',
+    title: 'Angel'
+}
+
+const getSaludo = () => {
+    return "Hola compis"
+}
+
+
+
+
+export const App = ({ title }) => {
+    return (
+        <>
+            <h1> { JSON.stringify( newMessage ) } </h1>
+            <h2> { getSaludo() } </h2>
+            <h1> { title } </h1>
+            {/* <h1>Hello World App</h1> */}
+            <p>parrafo</p>
+        </>
+    )
+}
+
+// Aqui ocupamos el prop-types que son para dar como una validacion nos mandara un error en consola
+// sino cumple con las validaciones por ejemplo debe ser string y obligatorio
+App.propTypes = {
+    title:PropTypes.string.isRequired
+}
+
+// Valores por defecto de una prop
+App.defaultProps = {
+    title: 'No hay titulo'
 }
--- counter-app1/src/main.jsx
+++ counter-app1/src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './FirstApp';
+import {CounterApp} from './CounterApp';
+
+// Importacion de estilos globales
+import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <App />
+        {/* el title es una property que le asignamos en el componente padre para pasarlo al coomponente hijo*/}
+        {/* <App title="Hola soy Goku" /> */}
+        <CounterApp value={20}/>
     </React.StrictMode>
 );
