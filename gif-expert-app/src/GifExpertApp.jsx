10eeb92d9cd1f188e38c314ff80ae43634ac9427
Angel Gabriel Muñoz Chavez <angel.chavez.amo@gmail.com>
2023-06-18 16:51:02 -0600

    update GifExpert

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
+
 export const GifExpertApp = () => {
+
+    const [ categories, setCategories ] = useState(['One Punch'])
+
     return (
         <>
             <h1>GifExpertApp</h1>
+
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={ category }>{ category }</li>
+                    })
+                }
+            </ol>
+
         </>
     )
 }
