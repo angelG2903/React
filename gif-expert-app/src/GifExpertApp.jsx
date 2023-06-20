1536b5432f0d0c9076d45a58850d921159175521
Angel Gabriel Muñoz Chavez <angel.chavez.amo@gmail.com>
2023-06-19 20:14:33 -0600

    Component Category

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -1,12 +1,34 @@
 import { useState } from "react"
+import { AddCategory } from './components/AddCategory'
+
 
 export const GifExpertApp = () => {
 
-    const [ categories, setCategories ] = useState(['One Punch'])
+    const [ categories, setCategories ] = useState(['One Punch','Dragon ball'])
+
+    // Agregamos una nueva categoria 2 formas
+    const onAddCategory = (newCategory) => {
+
+        if ( categories.includes(newCategory) ) return
+
+        setCategories([newCategory, ...categories] )
+        // setCategories( cat => [...cat, 'Valorant'] )
+    }
 
     return (
         <>
             <h1>GifExpertApp</h1>
+
+            <AddCategory 
+            // Masomenos
+                // setCategories={ setCategories } 
+
+            // Mejor 2 maneras de hacerlo
+                onNewCategory={ (value) => onAddCategory(value) }
+                // onNewCategory={ onAddCategory }
+            />
+
+            {/* <button onClick={ onAddCategory }>Agregar</button> */}
 
             <ol>
                 {
