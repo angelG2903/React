a3ff33d4af4d6faf13deef7cee3bd642f0d2a9d5
angelG2903 <angel.gabriel-1999@hotmail.com>
2023-06-24 15:44:07 -0600

    Hooks

---
+++ hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react"
+
+export const SimpleForm = () => {
+
+    const [formSatate, setFormSatate] = useState({
+        username: 'strider',
+        email: 'fernando@gmail.com'
+    })
+
+    const { username, email } = formSatate
+
+    const onInputChange = ({ target }) => {
+        const {name, value} = target
+        setFormSatate({
+            ...formSatate,
+            [name]: value
+        })
+    }
+
+    useEffect( () => {
+        console.log('useEffect called')
+    }, [])
+
+    useEffect( () => {
+        console.log('formSatate change')
+    }, [formSatate])    
+
+    useEffect( () => {
+        console.log('email change')
+    }, [email])    
+
+
+  return (
+    <>
+        <h1>SimpleForm</h1>
+        <hr />
+
+        <input 
+            type="text" 
+            className="form-control"
+            placeholder="Username"
+            name="username"
+            value={ username }
+            onChange={ onInputChange }
+        />
+
+        <input 
+            type="email"
+            className="form-control mt-2"
+            placeholder="fernando@gmail.com"
+            name="email"
+            value={ email }
+            onChange={ onInputChange }
+        />
+    </>
+  )
+}
